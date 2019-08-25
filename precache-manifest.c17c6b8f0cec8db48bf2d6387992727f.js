fbd905e02b45b0e2593f983019d952e38fd23b5b
ihargit <ihar.kharashun@gmail.com>
2019-08-25 21:57:20 +0300

    Updates

---
+++ precache-manifest.c17c6b8f0cec8db48bf2d6387992727f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa83c71fd5f77df696232e8b597062c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "275114598dfe6bf12129",
+    "url": "/todo-list/static/js/2.9f73500c.chunk.js"
+  },
+  {
+    "revision": "a6e1c4c283d146e1a06b",
+    "url": "/todo-list/static/js/main.076d0788.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
